1bd86d2faba91caa202525dac7abe459409028f1
sukmatria <129355178+sukmatria@users.noreply.github.com>
2023-05-29 07:11:38 +0700

    Add files via upload

---
+++ minggu9/RunHP.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com.mycompany.minggu9;
+
+/**
+ *
+ * @author Aspire
+ */
+public class RunHP {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
